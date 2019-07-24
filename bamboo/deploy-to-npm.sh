3192f1e690ab84a46e238aa1502904604f224327
Jonathan Kovarik <kovarik@nsidc.org>
2019-07-24 17:51:19 -0600

    Testing CI [skip-audit]

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -5,19 +5,17 @@ set -ex
 
 export VERSION=$(jq --raw-output .version lerna.json)
 export NPM_TAG=$(node ./bamboo/npm-tag.js);
+export NPM_TOKEN=''
 
 echo "Disabled because publish already occurred"
-# echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-# npx lerna publish \
-#   ${VERSION} \
-#   --no-git-tag-version \
-#   --no-push \
-#   --yes \
-#   --force-publish=* \
-#   --dist-tag=${NPM_TAG} \
-#   --exact
+echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
+npm_config_unsafe_perm=true; npx lerna publish \
+  ${VERSION} \
+  --no-git-tag-version \
+  --no-push \
+  --yes \
+  --force-publish=* \
+  --dist-tag=${NPM_TAG} \
+  --exact
+  --no-verify-access
 
-echo "Running build for testing purposes"
-
-cd packages/deployment
-npm run build
