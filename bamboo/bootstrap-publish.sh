9602ef4121bc6a906359fa4e20ab4f36d2e25504
Jonathan Kovarik <kovarik@nsidc.org>
2019-07-24 17:11:16 -0600

    Revert "Testing CI [skip-audit]"
    This reverts commit 3052defc06dcc45d006496901f964c42e89f6dfb.

--- bamboo/bootstrap-publish.sh
+++ bamboo/bootstrap-publish.sh
@@ -11,7 +11,7 @@ rm -rf website/build
 #npm run docs-build
 npm run bootstrap-no-build
 ls -ltra ./packages/
-npx lerna run prepare
+npx lerna prepare
 set +e; apt-get update; set -e;
 apt-get install -y jq rsync
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
