062d6bc8da5915a1d97453714feea2c0e3bf8760
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-22 16:31:32 -0600

    Add github_lambda_source

---
+++ tf-modules/github_lambda_source/variables.tf
@@ -0,0 +1,20 @@
+variable "release" {
+  type = string
+  default = null
+}
+variable "archive" {
+  type = string
+  default = null
+}
+variable "repo" {
+  type = string
+  default = null
+}
+variable "zip_file" {
+  type = string
+  default = null
+}
+variable "local_core_lambda" {
+  type = bool
+  default = false
+}
