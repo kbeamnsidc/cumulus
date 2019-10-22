9582b4eccdd1c230fa59ee6ee6cf062d1f2228f8
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-22 17:03:40 -0600

    WIP

--- tf-modules/github_lambda_source/main.tf
+++ tf-modules/github_lambda_source/main.tf
@@ -2,3 +2,7 @@ data "external" "github" {
     count = var.local_core_lambda ? 0 : 1
     program = ["curl", "-f", "-o ${var.archive}", "https://github.com/${var.repo}/releases/download/${var.release}/${var.zip_file}"]
 }
+
+output "result" {
+    value = external.github.result
+}
