425ac1fda0779924e3976546ce4f991312429934
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-22 21:43:50 -0600

    Update example lambdas to use data source

--- example/cumulus-tf/lambdas.tf
+++ example/cumulus-tf/lambdas.tf
@@ -1,7 +1,13 @@
+data "archive_file" "async_operation" {
+  type = "zip"
+  source_dir = "${path.module}/../../lambdas/asyncOperations/"
+  output_path = "${path.module}/../../lambdas/asyncOperations/lambda.zip"
+}
+
 resource "aws_lambda_function" "async_operation_fail" {
   function_name    = "${var.prefix}-AsyncOperationFail"
   filename         = "${path.module}/../../lambdas/asyncOperations/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/../../lambdas/asyncOperations/lambda.zip")
+  source_code_hash = "${data.archive_file.async_operation.output_base64sha256}"
   handler          = "index.fail"
   role             = module.cumulus.lambda_processing_role_arn
   runtime          = "nodejs8.10"
@@ -13,11 +19,10 @@ resource "aws_lambda_function" "async_operation_fail" {
     security_group_ids = [aws_security_group.no_ingress_all_egress.id]
   }
 }
-
 resource "aws_lambda_function" "async_operation_success" {
   function_name    = "${var.prefix}-AsyncOperationSuccess"
   filename         = "${path.module}/../../lambdas/asyncOperations/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/../../lambdas/asyncOperations/lambda.zip")
+  source_code_hash = "${data.archive_file.async_operation.output_base64sha256}"
   handler          = "index.success"
   role             = module.cumulus.lambda_processing_role_arn
   runtime          = "nodejs8.10"
@@ -30,10 +35,16 @@ resource "aws_lambda_function" "async_operation_success" {
   }
 }
 
+data "archive_file" "sns_s3_test" {
+  type = "zip"
+  source_dir = "${path.module}/../../lambdas/snsS3Test/"
+  output_path = "${path.module}/../../lambdas/snsS3Test/lambda.zip"
+}
+
 resource "aws_lambda_function" "sns_s3_test" {
   function_name    = "${var.prefix}-SnsS3Test"
   filename         = "${path.module}/../../lambdas/snsS3Test/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/../../lambdas/snsS3Test/lambda.zip")
+  source_code_hash = "${data.archive_file.sns_s3_test.output_base64sha256}"
   handler          = "index.handler"
   role             = module.cumulus.lambda_processing_role_arn
   runtime          = "nodejs8.10"
