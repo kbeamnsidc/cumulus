43e93facff75f0ab2ad5d9b45077b60e07c41e69
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-22 22:30:23 -0600

    update github_lambda_source

--- tf-modules/github_lambda_source/main.tf
+++ tf-modules/github_lambda_source/main.tf
@@ -1,4 +1,9 @@
-data "external" "github" {
-    count = var.local_core_lambda ? 0 : 1
-    program = ["curl", "-f", "-o", "${var.archive}", " https://github.com/${var.repo}/releases/download/${var.release}/${var.zip_file}"]
+resource "null_resource" "get_newest_CMA" {
+  count = var.local_core_lambda ? 0 : 1
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "curl -f, -o , ${var.archive},  https://github.com/${var.repo}/releases/download/${var.release}/${var.zip_file}"]
+  }
 }
