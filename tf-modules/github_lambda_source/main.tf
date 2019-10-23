e6ac084f6726158b0cbf921effd7109eabd6dee0
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-22 22:31:29 -0600

    Fix module definition

--- tf-modules/github_lambda_source/main.tf
+++ tf-modules/github_lambda_source/main.tf
@@ -4,6 +4,6 @@ resource "null_resource" "get_newest_CMA" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "curl -f, -o , ${var.archive},  https://github.com/${var.repo}/releases/download/${var.release}/${var.zip_file}"]
+    command = "curl -f, -o , ${var.archive},  https://github.com/${var.repo}/releases/download/${var.release}/${var.zip_file}"
   }
 }
