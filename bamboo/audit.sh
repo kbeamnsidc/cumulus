746139a4718dbada79a972eed25ab8526cc1add9
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-31 09:08:57 -0600

    Disable all tests prior to integration tests

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
 set -ex
-. ./bamboo/abort-if-not-pr-or-master.sh
-. ./bamboo/set-bamboo-env-variables.sh
-
-commit_message_contains_skip_audit_flag=false
-commit_matches_version_tag=false
-
-if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
-  commit_message_contains_skip_audit_flag=true;
-fi
-if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
-  commit_matches_version_tag=true;
-fi
-
-if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false && $SKIP_AUDIT != true ]]; then
-  npm run install-locks;
-  npm run audit;
-else
-  >&2 echo "******Skipping audit due to commit message/version tag/env var being present"
-fi
--- bamboo/lint.sh
+++ bamboo/lint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -ex
 . ./bamboo/abort-if-not-pr-or-master.sh
-
-npm install
-npm run bootstrap-no-build
-npm run lint
