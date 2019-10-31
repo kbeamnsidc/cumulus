d7207a12fd48b1e5ae825ca3df8044d2e753911b
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-31 13:05:05 -0600

    Revert "Disable bootstrapping for this test set"
    This reverts commit a806069634bb7795e0c2b6a11899d53304b3a960.

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -14,7 +14,13 @@ npm config set unsafe-perm true
 npm install
 . ./bamboo/set-bamboo-env-variables.sh
 
-
+if [[ $USE_NPM_PACKAGES == true ]]; then
+  echo "***Deploying stack with NPM packages"
+  (cd example && npm install)
+else
+  echo "***Deploying stack with built packages"
+  npm run bootstrap
+fi
 
 echo "Locking stack for deployment $DEPLOYMENT"
 
@@ -42,3 +48,12 @@ if [[ $LOCK_EXIST_STATUS -gt 0 ]]; then
   exit 1
 fi
 set -e
+
+if [[ $DEPLOYMENT =~ '-tf' ]]; then
+  echo "Running Terraform deployment $DEPLOYMENT"
+  . ../bamboo/abort-if-not-terraform.sh
+  . ../bamboo/bootstrap-tf-deployment.sh
+else
+  echo "Running Kes deployment $DEPLOYMENT"
+  . ../bamboo/bootstrap-kes-deployment.sh
+fi
