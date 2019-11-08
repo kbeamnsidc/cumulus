cb6a54b23143bfb58db3b3295890cd1b04aaf84f
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-11-08 06:47:11 -0700

    Add test configuration for bamboo test builds

--- bamboo/select-stack.js
+++ bamboo/select-stack.js
@@ -15,22 +15,7 @@ function determineIntegrationTestStackName(cb) {
   if (branch === 'master') return cb('cumulus-from-source');
 
   const stacks = {
-    'Aimee Barciauskas': 'aimee',
-    'Jenny Liu': 'jl',
-    jennyhliu: 'jl',
-    kkelly51: 'kk',
-    'Lauren Frederick': 'lf-int-test',
-    laurenfrederick: 'lf-int-test',
-    'Mark Boyd': 'mboyd-int',
-    Marc: 'mth',
-    yjpa7145: 'mth',
-    mhuffnagle: 'mth',
-    'Marc Huffnagle': 'mth',
-    'Matt Savoie': 'mhs',
-    'Jonathan Kovarik': 'jk',
-    'Menno Van Diermen': 'mvd',
-    'Jacob Campbell': 'jc',
-    'Chuck Daniels': 'chuckulus-ci'
+    'Jonathan Kovarik': 'bamboo'
   };
 
   return git('.').log({ '--max-count': '1' }, (e, r) => {
@@ -41,7 +26,7 @@ function determineIntegrationTestStackName(cb) {
     if (author && stacks[author]) {
       return cb(stacks[author]);
     }
-    return cb('cumulus-from-pr');
+    return cb('bamboo');
   });
 }
 
